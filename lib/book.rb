8e06a77b686dfe9f0bd5a7233b2e49a6ace4dee2
Alison Shin <alisonyshin@gmail.com>
2018-08-17 13:41:29 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,11 @@
 class Book
-  attr_accessor :title, :author, :page_count
+  attr_accessor :author, :page_count
   
   @@genres = []
+  
+  def initialize(title)
+    title = title
+  end
   
 end
 
